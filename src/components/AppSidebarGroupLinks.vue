4a0ab52313286c8ba4c0574e233216a6541b665d
Ali hadi <76496433+Ali-hadi1@users.noreply.github.com>
2021-03-05 08:55:52 -0800

    add link and group links

---
+++ src/components/AppSidebarGroupLinks.vue
@@ -0,0 +1,38 @@
+<template>
+    <ul
+        class="my-1 px-3 py-2 list-none flex flex-col  justify-between items-center hover:bg-link-hover rounded cursor-pointer text-white"
+        :class="{'bg-link-active':dropDownState}"
+    >
+        <li class="w-full flex justify-between items-center text-sm font-bold text-gray-300" @click="dropDownLinks">
+            {{title}}
+            <svg class="w-3 h-3 font-extrabold" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 15l7-7 7 7" />
+            </svg>
+        </li>
+        <ul class="w-full" v-if="dropDownState">
+            <slot name="links"></slot>
+        </ul>
+    </ul>
+</template>
+<script>
+import { mapState } from 'vuex'
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true
+    }
+  },
+  methods: {
+    dropDownLinks () {
+      this.$store.commit('SET_DROPDOWNLINKSTATE')
+    }
+  },
+  computed: mapState({
+    dropDownState: state => state.dropDownState
+  })
+}
+</script>
+<style scoped>
+
+</style>
---
+++ src/components/AppSidebarLink.vue
@@ -0,0 +1,54 @@
+<template>
+  <li class="w-full my-1 box-border">
+    <router-link
+        v-bind="$attrs"
+        :to="{name:routeName}"
+        exact
+        class="w-full block px-3 list-none hover:bg-link-hover rounded text-sm cursor-pointer text-gray-300"
+        :class="[paddingClass,textHoverClass , fontWeight]"
+        :active-class="activeClass"
+    >
+        {{title}}
+    </router-link>
+  </li>
+</template>
+<script>
+export default {
+  inheritAttrs: true,
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    routeName: {
+      type: String,
+      required: true
+    },
+    paddingClass: {
+      type: String,
+      default: 'py-2'
+    },
+    textHoverClass: {
+      type: String,
+      default: ''
+    },
+    fontWeight: {
+      type: String,
+      default: 'font-bold'
+    },
+    activeClass: {
+      type: String,
+      default: 'bg-link-active'
+    }
+  },
+  data: function () {
+    return {
+
+    }
+  }
+
+}
+</script>
+<style scoped>
+
+</style>
