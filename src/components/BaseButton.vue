d5522c8f6d9ecdaeb5d0afeebf8d8a41f30aac4c
Ali hadi <76496433+Ali-hadi1@users.noreply.github.com>
2021-03-05 08:54:39 -0800

    add base widget to project

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="mt-2">
+    <button
+      :type="type"
+      :class="[buttonClass, buttonFloat]"
+      class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-sm font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:outline-none focus:border-gray-900 focus:shadow-outline-gray transition ease-in-out duration-150"
+    >
+      <slot />
+    </button>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    type: {
+      type: String,
+      defualt: 'submit'
+    },
+    buttonClass: {
+      type: String
+    },
+    buttonFloat: {
+      type: String,
+      defualt: 'float-left'
+    }
+  }
+}
+</script>
---
+++ src/components/BaseInput.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="pt-2">
+    <label v-if="label">{{ label }}</label>
+    <input
+      :type="type"
+      :value="value"
+      @input="updateValue"
+      v-bind="$attrs"
+      class="px-2 py-1 rounded-sm focus:outline-none focus:border-purple-300 border border-gray-400"
+      :class="[widthClass]"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  inheritAttrs: false,
+  props: {
+    label: {
+      type: String,
+      defualt: ''
+    },
+    type: {
+      type: String,
+      defualt: 'text'
+    },
+    value: [String, Number],
+    widthClass: {
+      type: String
+    }
+  },
+  data: function () {
+    return {}
+  },
+  methods: {
+    updateValue (event) {
+      this.$emit('input', event.target.value)
+    }
+  }
+}
+</script>
