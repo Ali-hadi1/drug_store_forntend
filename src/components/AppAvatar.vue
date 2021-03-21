5f25aed9b548b847ba400195c2e80f09e0d1a000
Ali hadi <76496433+Ali-hadi1@users.noreply.github.com>
2021-03-21 13:03:01 -0700

    changes in user icon pc and dropdown of it

--- src/components/AppAvatar.vue
+++ src/components/AppAvatar.vue
@@ -1,19 +1,24 @@
 <template>
   <div class="relative ">
-    <div class="h-5 w-5 rounded-full overflow-hidden bg-indigo-600">
-      <img v-bind:src="null" alt="Avatar" class="h-8 w-auto object-cover"/>
+    <div
+      class="h-5 w-5 rounded-full overflow-hidden bg-indigo-600"
+      @click="toggleVisibility"
+      @keydown.exact.space.prevent="toggleVisibility"
+    >
+    <a href="#">
+      <img
+      v-bind:src="null"
+      alt="Avatar"
+      class="h-8 w-auto object-cover"
+      />
+    </a>
     </div>
 
-    <div class="absolute -right-1 bg-gray-100 w-44 rounded py-2 mt-2">
+    <div v-if="isVisible" class="absolute -right-1 bg-gray-100 w-44 rounded py-2 mt-2">
       <ul class="w-full">
         <li class="dropdown-items">
-          <font-awesome-icon icon="home"/>
-        </li>
-        <li class="dropdown-items">
-          hadi
-        </li>
-        <li class="py-1 pl-4 text-sm">
-          hadi
+          <font-awesome-icon icon="user-circle"/>
+          <span></span>
         </li>
       </ul>
     </div>
@@ -22,7 +27,16 @@
 
 <script>
 export default {
-
+  data () {
+    return {
+      isVisible: false
+    }
+  },
+  methods: {
+    toggleVisibility () {
+      this.isVisible = !this.isVisible
+    }
+  }
 }
 </script>
 
