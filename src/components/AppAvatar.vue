51d6c10c05334aecd019ddb88b0477bbd51056d1
Ali hadi <76496433+Ali-hadi1@users.noreply.github.com>
2021-03-22 11:27:50 -0700

    add user Avatar to Navbar

--- src/components/AppAvatar.vue
+++ src/components/AppAvatar.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="relative ">
     <div
-      class="h-5 w-5 rounded-full overflow-hidden bg-indigo-600"
+      class="h-6 w-6 rounded-full overflow-hidden bg-indigo-600"
       @click="toggleVisibility"
       @keydown.exact.space.prevent="toggleVisibility"
     >
@@ -14,11 +14,29 @@
     </a>
     </div>
 
-    <div v-if="isVisible" class="absolute -right-1 bg-gray-100 w-44 rounded py-2 mt-2">
+    <div v-if="isVisible" class="absolute -right-1 top-8 bg-gray-100 w-44 rounded">
       <ul class="w-full">
         <li class="dropdown-items">
-          <font-awesome-icon icon="user-circle"/>
-          <span></span>
+          <app-sidebar-link
+            icon="user-circle"
+            title="Profile"
+            routeName="Profile"
+            fontWeight="font-thin"
+            textHoverClass="hover:text-blue-400"
+            activeClass="text-gray-700"
+            textColor="text-gray-700"
+          >
+
+          </app-sidebar-link>
+        </li>
+        <li class="dropdown-items">
+          <button
+            @click.prevent="logout"
+            class="focus:outline-none w-full h-full flex gap-1 items-center justify-start px-3 py-2 font-thin text-gray-700 hover:text-blue-400"
+          >
+            <font-awesome-icon icon="sign-out-alt"/>
+            logout
+          </button>
         </li>
       </ul>
     </div>
@@ -26,7 +44,9 @@
 </template>
 
 <script>
+import AppSidebarLink from './AppSidebarLink.vue'
 export default {
+  components: { AppSidebarLink },
   data () {
     return {
       isVisible: false
@@ -35,6 +55,9 @@ export default {
   methods: {
     toggleVisibility () {
       this.isVisible = !this.isVisible
+    },
+    logout () {
+      this.$store.dispatch('logout')
     }
   }
 }
