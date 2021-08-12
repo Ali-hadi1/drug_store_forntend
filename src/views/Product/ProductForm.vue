aa60514367e7b0617b9181a779066115fc7a9720
Ali hadi <76496433+Ali-hadi1@users.noreply.github.com>
2021-08-12 12:16:32 -0700

    add new Product form component.

---
+++ src/views/Product/ProductForm.vue
@@ -0,0 +1,17 @@
+<template>
+  <app>
+    <h1>the Form come here !</h1>
+  </app>
+</template>
+
+<script>
+import App from '@/components/layouts/App.vue'
+export default {
+  components: { App },
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
